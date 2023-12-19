b651205b48980b16cc8db5706fca9e4ffb3745e4
Bolang-Lonson <bbeinyoh12@gmail.com>
2023-12-19 16:11:23 +0100

    third assignment

--- ass3.py
+++ ass3.py
@@ -1,2 +1,12 @@
-def hypotenuse(leg1, leg2):
-    pass
+def hypotenuse(leg1: float, leg2: float) -> float:
+    a = leg1 ** 2
+    b = leg2 ** 2
+    c = (a + b) ** 0.5
+    return c
+
+# print(hypotenuse(3, 4))
+print(hypotenuse(5, 12))
+print(hypotenuse(1, 1))
+
+n = int(10.)
+print(isinstance(n, float), isinstance(n * 1.0, float))
