378819ec16317752f6ad8fb505668802de291c57
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-05-01 17:16:36 +0100

    learned typecasting

---
+++ Programming 1/src/TypeCasting.java
@@ -0,0 +1,12 @@
+import textio.TextIO;
+
+public class TypeCasting {
+    public static void main(String args[]) {
+
+        // Printing the letters of the alphabet
+        
+        for (char letter = 'A'; letter <= 'z'; letter++) {
+            TextIO.putln(letter);
+        }
+    }
+}
