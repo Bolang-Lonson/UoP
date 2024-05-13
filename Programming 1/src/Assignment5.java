b8144fe89bc2393e0bcc141eb967ebbbef09f5d1
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-05-13 13:41:21 +0100

    Completed assignment. It was a long and tedious one

--- Programming 1/src/Assignment5.java
+++ Programming 1/src/Assignment5.java
@@ -1,8 +1,13 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
+import textio.TextIO;
 
+/**
+ * Class which eventually implements the Administrator interface
+  */
 public class Assignment5 {
-
+    public static void main(String[] args) {
+        CourseManagement.start();
+    }
 }
 
 /**
@@ -41,12 +46,16 @@ class Student {
     }
 
     public void enroll(Course course) {
-        if (course.isFull) {
+        if (course.isFull()) {
             System.out.println("Error: Course " + course.getCode() + " is full already");
             return;
         }
         enrolledCourses.add(course);
         course.addStudent();
+    }
+
+    public HashMap<Course, String> getGrades() {
+        return grades;
     }
 
     public void assignGrade(Course course, String grade) {
@@ -60,7 +69,7 @@ class Course {
     private static int numberEnrolled = 0;
 
     public Course(String code, String name, int maxCapacity) {
-        this.courseCode = code;
+        this.code = code;
         this.name = name;
         this.maxCapacity = maxCapacity;
         numberEnrolled++;
@@ -93,6 +102,7 @@ class Course {
 
 class CourseManagement {
     private static ArrayList<Course> courses = new ArrayList<Course>();
+    private static HashMap<Student, HashMap<Course, String>> studentGrades = new HashMap<>();
 
     public static void addCourse(String code, String name, int maxCapacity) {
         courses.add(new Course(code, name, maxCapacity));
@@ -104,5 +114,98 @@ class CourseManagement {
 
     public static void assignGrade(Student student, Course course, String grade) {
         student.assignGrade(course, grade);
+    }
+
+    public static String calcOverallGrade(Student student) {
+        HashMap<Course, String> grades = student.getGrades();
+        for (String grade: grades.values()) {
+            // implement averaging logic for grades here.
+            // grading system not explicitly given or known
+        }
+    }
+
+    public static String overallCourseGrade(String courseCode) {
+        for(Course course: courses) {
+            if (course.getCode().equals(courseCode)) {
+                for(HashMap<Course, String> courseAndGrade: studentGrades.values()){
+                    String grade = courseAndGrade.get(course);
+                    // logic to get overall course grade
+                }
+            }
+        }
+    }
+
+    public static void start(){
+        int option;
+
+        do {
+            System.out.println("Course Enrollment and Grade Management System");
+            System.out.println("1.\tAdd New Course\n2.\tEnroll Student\n3.\tAssign Grades\n4.\tCalculate Overall Grades\n5.\tExit");
+
+            System.out.print("Enter you option");
+
+            // using TextIO's in-built error handling system
+            option = TextIO.getlnInt();
+            switch (option) {
+                case 1 -> {
+                    System.out.println("Enter course");
+                    System.out.print("Code:\t");
+                    String code = TextIO.getln();
+                    System.out.print("Name:\t");
+                    String name = TextIO.getln();
+                    System.out.print("Maximum Capacity:\t");
+                    int maxCapacity = TextIO.getInt();
+                    System.out.println();
+                    addCourse(code, name, maxCapacity);
+                }
+                case 2 -> {
+                    System.out.println("Enter student's");
+                    System.out.print("Name:\t");
+                    String name = TextIO.getln();
+                    System.out.print("Id:\t");
+                    String id = TextIO.getln();
+                    
+                    System.out.println("Enter course code:");
+                    String code = TextIO.getln();
+                    for(Course course: courses) {
+                        if (course.getCode().equals(code)) {
+                            enrollStudent(new Student(name, id), course);
+                            break;
+                        }
+                    }
+                    System.out.println("Error: Course doesn't exist in course registry");
+                }
+                case 3 -> {
+                    System.out.println("Enter student's");
+                    System.out.print("Name:\t");
+                    String name = TextIO.getln();
+                    System.out.print("Id:\t");
+                    String id = TextIO.getln();
+
+                    System.out.println("Enter course code:");
+                    String code = TextIO.getln();
+                    for(Course course: courses) {
+                        if (course.getCode().equals(code)) {
+                            System.out.print("Grade:\t");
+                            String grade = TextIO.getln();
+                            assignGrade(new Student(name, id), course, grade);
+                            break;
+                        }
+                    }
+                    System.out.println("Error: Course doesn't exist in course registry");
+                }
+                case 4 -> {
+                    System.out.println("Enter course code for course:");
+                    String code = TextIO.getln();
+                    System.out.println("Overall course grade is: " + overallCourseGrade(code));
+                }
+                case 5 -> {
+                    System.out.println("Exiting program!...");
+                }
+                default -> {
+                    System.out.println("Invalid Choice.");
+                }
+            }
+        } while (option != 5);
     }
 }
