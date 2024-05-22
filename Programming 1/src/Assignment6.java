9ba28b93fe0e199f10872a6ef4b2dbf579834215
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-05-23 00:40:26 +0100

    motorcycle and car class created

--- Programming 1/src/Assignment6.java
+++ Programming 1/src/Assignment6.java
@@ -60,6 +60,41 @@ public class Car implements Vehicle, CarVehicle {
     public String getFuelType() { return fuelType; }
 }
 
+public class Motorcycle implements Vehicle, MotorVehicle {
+    private String make;
+    private String model;
+    private int yearOfManufacture;
+    private int numberOfWheels;
+    private String type;
+
+    public Motorcycle(String make, String model, int yearOfManufacture) {
+        this.make = make;
+        this.model = model;
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    @Override
+    public String getMake() { return make; }
+
+    @Override
+    public String getModel() { return model; }
+
+    @Override
+    public int getYearOfManufacture() { return yearOfManufacture; }
+
+    @Override
+    public void setNumberOfWheels(int wheels) { this.numberOfWheels = wheels; }
+
+    @Override
+    public int getNumberOfWheels() { return numberOfWheels; }
+
+    @Override
+    public void setType(String type) { this.type = type; }
+
+    @Override
+    public String getType() { return type; }
+}
+
 public class Assignment6 {
 
 }
