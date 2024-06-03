1ed2b09019b4feb7f867d0e703e73c67f22df966
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-06-03 10:46:13 +0100

    the stream API in java for parallelisation of computations

---
+++ Programming 1/src/StreamAPI.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.stream.*;
+
+public class StreamAPI {
+    public static void main(String[] args) {
+        ArrayList<String> stringlList = new ArrayList<>();
+
+        // using a parallel stream to compute the average length of strings in the arraylist
+        int lengthSum = stringList.parallelStream()
+                                  .mapToInt( str -> str.length() )  // The result of the map operation is to produce a new stream, this time a stream of integers, consisting of all the outputs from the function.
+                                  .sum();
+        //  The fact that it is a “parallelStream” makes it possible to parallelize the computation.
+        double average = (double)lengthSum / stringList.size();
+    }
+}
