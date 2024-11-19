40b00c3a22186beae491d4eeb475c8d6d280fae5
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-11-19 18:03:45 +0100

    Exceptions and exception handling

---
+++ Programming 2/src/L01TryCatch.java
@@ -0,0 +1,48 @@
+import textio.TextIO;
+
+public class L01TryCatch {
+    public static void main(String[] args) throws Exception {
+        String str = "hi";
+        double x;
+
+        try {
+            x = Double.parseDouble(str);
+            System.out.println("The number is " + x);
+        } catch (NumberFormatException e) {
+            // TODO: handle exception
+            System.out.println("Not a legal number." + e);
+            x = Double.NaN;
+        }
+
+        computeAverage();
+    }
+
+    static void computeAverage() {
+        String str; // The user’s input.
+        double number; // The input converted into a number.
+        double total; // The total of all numbers entered.
+        double avg; // The average of the numbers.
+        int count; // The number of numbers entered.
+        total = 0;
+        count = 0;
+        System.out.println("Enter your numbers, press return to end.");
+        while (true) {
+            System.out.print("? ");
+            str = TextIO.getln();
+            if (str.equals("")) {
+                break; // Exit the loop, since the input line was blank.
+            }
+            try {
+                number = Double.parseDouble(str);
+                // If an error occurs, the next 2 lines are skipped!
+                total = total + number;
+                count = count + 1;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Not a legal number! Try again.");
+            }
+        }
+        avg = total/count;
+        System.out.printf("The average of %d numbers is %1.6g%n", count, avg);
+    }
+}
